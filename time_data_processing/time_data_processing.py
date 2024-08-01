35697312c5606fb312bdf30145e1b5be8b44a330
cgug_aa <cksrn0003@naver.com>
2024-08-01 17:47:51 +0900

    240801

--- time_data_processing/time_data_processing.py
+++ time_data_processing/time_data_processing.py
@@ -7,20 +7,28 @@ from collections import defaultdict
 
 # 1st: 차량 이동 경로 불러오기
 def data_processing(data_directory):
-    data_path=data_directory
-    usr_name=data_path.split('\\')[-1]
-    date=[]
-    start_time=[]
-    end_time=[]
-    for file_name in os.listdir(data_path):
-        if file_name.endswith('.csv'):
-            raw_data=pd.read_csv(os.path.join(data_path, file_name))
-            start, end=to_datetime(raw_data)
-            start_time.append(start)
-            end_time.append(end)
-            date.append(file_name[:-4])
-    make_new_csv(usr_name, date, start_time, end_time)
+    total_start_time=[]
+    total_end_time=[]
+    for directory_name in os.listdir(data_directory):                                           #17562개 데이터
+        dir_path=os.path.join(data_directory, directory_name)
+        if (os.path.isdir(dir_path) == True) and (directory_name != 'preprocessing_time'):
+            date=[]
+            start_time=[]
+            end_time=[]
+            for file_name in os.listdir(dir_path):
+                if file_name.endswith('.csv'):
+                    raw_data=pd.read_csv(os.path.join(dir_path, file_name))
+                    start, end=to_datetime(raw_data)
+                    start_time.append(start)
+                    end_time.append(end)
+                    total_start_time.extend(start_time)
+                    total_end_time.extend(end_time)
+                    date.append(file_name[:-4])
+            # csv 파일 생성
+            make_new_csv(directory_name, date, start_time, end_time)
 
+    # start~end 사이에 포함되는 모든 시간을 카운팅한 csv 파일 생성
+    count_time_label(start_time, end_time)
 
 # 2nd: 시간스탬프 변환하기
 def to_datetime(data_csv):
@@ -33,7 +41,8 @@ def to_datetime(data_csv):
 def make_new_csv(usr_name, list1, list2, list3):
     dataframe=zip(list1, list2, list3)
     new_dataframe=pd.DataFrame(dataframe, columns=['date', 'start', 'end'])
-    new_dataframe['(start, end)'] = new_dataframe.apply(lambda row: (row['start'], row['end']), axis=1)
+
+    #new_dataframe['(start, end)'] = new_dataframe.apply(lambda row: (row['start'], row['end']), axis=1)
     
     #start와 end 칼럼, (start, end) 칼럼을 추가한 csv파일 생성
     new_file_path=os.getcwd()+'\\data\\위드라이브\\preprocessing_time\\'+usr_name
@@ -42,7 +51,8 @@ def make_new_csv(usr_name, list1, list2, list3):
     if not os.path.exists(new_file_path):  
         os.makedirs(new_file_path)
     new_dataframe.to_csv(new_file)
-    
+
+    '''
     #start와 end의 튜플쌍을 카운팅한 csv파일 생성
     count_file=os.getcwd()+'\\data\\위드라이브\\preprocessing_time\\'+usr_name+'\\count_'+usr_name+'.csv'
     data_dict=count_tuple(new_dataframe)
@@ -50,6 +60,7 @@ def make_new_csv(usr_name, list1, list2, list3):
     rows = [{'(start, end)': key, 'count': value} for key, value in data_dict.items()]
     data_dict_dataframe=pd.concat([data_dict_dataframe, pd.DataFrame(rows)], ignore_index=True)
     data_dict_dataframe.to_csv(count_file)
+
 
 
     #count_id.csv파일에 start와 end시간의 TL매칭하기
@@ -64,10 +75,15 @@ def make_new_csv(usr_name, list1, list2, list3):
     TL_matching_DataFrame_by4.to_csv(TL_matching_by4_path)
 
     #TL4, 3으로 분할한 csv 파일 만들기
-    label_count(usr_name, data_dict_dataframe)
+    #label_count(usr_name, data_dict_dataframe)
 
+    '''
 
-    
+    #new_dataframe에 TL에 따른 매칭 값 칼럼 추가
+    new_dataframe=TL_matching_csv(3, new_dataframe)
+    new_dataframe=TL_matching_csv(4, new_dataframe)
+    TL_matching_path=os.getcwd()+'\\data\\위드라이브\\preprocessing_time\\'+usr_name+'\\TL_matching_'+usr_name+'.csv'
+    new_dataframe.to_csv(TL_matching_path)
 
 # timelabel의 값을 기준으로 count하는 함수
 def count_tuple(dataframe):
@@ -81,10 +97,10 @@ def label_count(id, csv_dataframe):
 
     # 4분할 시간 범위 정의
     time_ranges_by4 = {
-        'label1': range(5, 11),   # 5 ~ 10시
-        'label2': range(11, 16),  # 11 ~ 15시
-        'label3': range(16, 20),  # 16시 ~ 19시
-        'label4': list(range(20, 24)) + list(range(0, 5))  # 20시 ~ 24시 및 0시 ~ 4시
+        'label1': range(7, 11),   # 7 ~ 10시
+        'label2': range(11, 15),  # 11 ~ 14시
+        'label3': range(15, 23),  # 15시 ~ 22시
+        'label4': list(range(23, 24)) + list(range(0, 7))  # 23시 ~ 0시 및 0시 ~ 6시
     }
     
     # 4분할 TL 카운트
@@ -107,9 +123,9 @@ def label_count(id, csv_dataframe):
     #---------------------------------------------------------------------
     #3분할 시간 범위 정의
     time_ranges_by3 = {
-        'label1': range(6, 11),   # 6 ~ 11시
-        'label2': range(12, 18),  # 12 ~ 18시
-        'label3': list(range(19, 24))+ list(range(0, 5))  # 19시 ~ 5시
+        'label1': range(11, 15),   # 11 ~ 14시
+        'label2': range(15, 23),  # 15 ~ 23시
+        'label3': list(range(23, 24))+ list(range(0, 11))  # 23시 ~ 10시
     }
 
     #3분할 시간 카운트
@@ -133,13 +149,15 @@ def label_count(id, csv_dataframe):
 def TL_matching_csv(label_num, dataframe):
     new_dataframe=dataframe.copy()
     if label_num==3:
-        T1_by3=range(0, 7)
-        T2_by3=range(7, 17)
-        T3_by3=range(17, 24)
-
+        T1_by3=list(range(23, 24)) + list(range(0, 11))
+        T2_by3=range(11, 15)
+        T3_by3=range(15, 23)
         start_TL=[]
         end_TL=[]
-        for start, end in dataframe['(start, end)']:
+
+        for row in new_dataframe.itertuples(index=False):
+            start = row.start
+            end = row.end
             if start in T1_by3: start_TL.append('T1')
             elif start in T2_by3: start_TL.append('T2')
             elif start in T3_by3: start_TL.append('T3')
@@ -148,17 +166,19 @@ def TL_matching_csv(label_num, dataframe):
             elif end in T2_by3: end_TL.append('T2')
             elif end in T3_by3 : end_TL.append('T3')
             else: raise TypeError
-        new_dataframe['TL_start_by3']=start_TL
-        new_dataframe['TL_end_by3']=end_TL
+        new_dataframe['TL_3_start']=start_TL
+        new_dataframe['TL_3_end']=end_TL
         return new_dataframe
     else:
-        T1_by4=range(0, 7)
-        T2_by4=range(7, 13)
-        T3_by4=range(13, 17)
-        T4_by4=range(17, 24)
+        T1_by4=list(range(23, 24)) + list(range(0, 7))
+        T2_by4=range(7, 11)
+        T3_by4=range(11, 15)
+        T4_by4=range(15, 23)
         start_TL=[]
         end_TL=[]
-        for start, end in dataframe['(start, end)']:
+        for row in new_dataframe.itertuples(index=False):
+            start = row.start
+            end = row.end
             if start in T1_by4: start_TL.append('T1')
             elif start in T2_by4: start_TL.append('T2')
             elif start in T3_by4: start_TL.append('T3')
@@ -169,11 +189,61 @@ def TL_matching_csv(label_num, dataframe):
             elif end in T3_by4 : end_TL.append('T3')
             elif end in T4_by4 : end_TL.append('T4')
             else: raise TypeError
-        new_dataframe['TL_start_by4']=start_TL
-        new_dataframe['TL_end_by4']=end_TL
+        new_dataframe['TL_4_start']=start_TL
+        new_dataframe['TL_4_end']=end_TL
         return new_dataframe
+
+
+#start 시간과 end 시간 내 존재하는 모든 시간 카운트하기
+def count_time_label(start_time, end_time):
+    count_time_dict={   '0' : 0,
+                        '1' : 0,
+                        '2' : 0,
+                        '3' : 0,
+                        '4' : 0,
+                        '5' : 0,
+                        '6' : 0,
+                        '7' : 0,
+                        '8' : 0,
+                        '9' : 0,
+                        '10' : 0,
+                        '11' : 0,
+                        '12' : 0,
+                        '13' : 0,
+                        '14' : 0,
+                        '15' : 0,
+                        '16' : 0,
+                        '17' : 0,
+                        '18' : 0,
+                        '19' : 0,
+                        '20' : 0,
+                        '21' : 0,
+                        '22' : 0,
+                        '23' : 0    }
+    for s, e in zip(start_time, end_time):
+        if s <= e:
+            time=[x for x in range(s, e+1)]
+            for i in time:
+                count_time_dict[str(i)]+=1
+        else:
+            time=[x for x in range(s, 24)]
+            time2=[x for x in range(e+1)]
+            time.extend(time2)
+            for i in time:
+                count_time_dict[str(i)]+=1
+
+    #csv 파일 경로
+    count_time_dict_csv=os.getcwd()+'\\data\\위드라이브\\preprocessing_time\\count_all_time.csv'
+
+    #csv 파일 생성
+    new_dataframe=pd.DataFrame(columns=['time', 'count'])
+    rows = [{'time': key, 'count': value} for key, value in count_time_dict.items()]
+    new_dataframe=pd.concat([new_dataframe, pd.DataFrame(rows)], ignore_index=True)
+    new_dataframe.to_csv(count_time_dict_csv)
+
 
 # 원본 데이터에서 시간스탬프를 시간시간과 끝시간을 추출하여 파일로 저장
 # 테스트 데이터 위드라이브(1be2e43d69994758973f6185bdd973d0)
-directory=os.getcwd()+'\\data\\위드라이브\\1be2e43d69994758973f6185bdd973d0'
+
+directory=os.getcwd()+'\\data\\위드라이브'
 data_processing(directory)
